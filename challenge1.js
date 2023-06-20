48c8fdbe81a37d6b7069ff5887f3a1a9ff39fc1f
Brian Manoti <brian.manoti@student.moringaschool.com>
2023-06-20 11:48:41 +0300

    complete assignment

--- challenge1.js
+++ challenge1.js
@@ -1,20 +1,4 @@
-function calculateGrade()
-const marks = parseInt(prompt("Enter student marks (between 0 and 100):"));
-
-let grade;
-if (marks >100 && marks <0 (marks)) {
-            grade = "invalid input"
-  
-    }else if (marks > 79) {
-    grade = "A";
-  } else if (marks >= 60) {
-    grade = "B";
-  } else if (marks >= 50) {
-    grade = "C";
-  } else if (marks >= 40) {
-    grade = "D";
-  } else {
-    grade = "E";
-  }
-
-  console.log("The student grade is>:"+grade);
+function generateGrade()
+let marks = parseFloat(prompt("Enter your grade:"))
+    if (marks <0)
+           
---
+++ challenge3.js
@@ -0,0 +1,5 @@
+function calculateNHIF(basicsalary){
+    if (basicsalary >=6000 && basicsalary <=7999){
+        return 300;
+    }
+}
